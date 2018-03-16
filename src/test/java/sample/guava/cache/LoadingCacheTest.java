c5a7db01c5e8f2aca998af16738f203053f7edbd
Ivar <ivar.chen@ge.com>
2018-03-16 17:56:33 +0800

    trival fixes

--- src/test/java/sample/guava/cache/LoadingCacheTest.java
+++ src/test/java/sample/guava/cache/LoadingCacheTest.java
@@ -7,7 +7,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
